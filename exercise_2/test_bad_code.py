4e32ac4c04b0bcb550cafc4ff083e72ae118d208
Pavel-2024 Vagner <pavvagner@csob.sk>
2024-06-11 11:29:32 +0000

    Exerc2_04

--- exercise_2/test_bad_code.py
+++ exercise_2/test_bad_code.py
@@ -3,5 +3,7 @@ from bad_code import basic_math
 
 def test_math_that_you_will_be_doing_at_work(capsys):
     test_variable = basic_math()
-    assert test_variable == 5
+    #assert test_variable == 5
+    # my correction
+    assert test_variable == 4
 
